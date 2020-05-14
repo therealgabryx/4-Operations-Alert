1efe8f68d2f69d264e59566068a6b7da457bbd93
Gabriele Concli <gabriele.concli@gmail.com>
2020-05-14 09:42:06 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,22 @@
+function exec() {
+  sum(6, 3);
+  subtract(6, 3);
+  multiply(6, 3);
+  divide(6, 3);
+}
+
+function sum(a, b) {
+  alert(`Somma: ${a} + ${b} = ${a + b}`);
+}
+
+function subtract(a, b) {
+  alert(`Somma: ${a} - ${b} = ${a - b}`);
+}
+
+function multiply(a, b) {
+  alert(`Somma: ${a} * ${b} = ${a * b}`);
+}
+
+function divide(a, b) {
+  alert(`Somma: ${a} / ${b} = ${a / b}`);
+}
